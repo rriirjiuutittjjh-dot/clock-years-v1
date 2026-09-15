4304d97b2addbe5c39f9f9e8118c99830762ea1c
rriirjiuutittjjh-dot <rriirjiuutittjjh@gmail.com>
2026-09-15 04:33:13 +0200

    Delete src directory

--- src/routes/login.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { AuthForm } from "@/components/auth/auth-form";
-
-export const Route = createFileRoute("/login")({ component: Login });
-
-function Login() {
-  return <AuthForm mode="login" />;
-}
