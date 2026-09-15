6316fe0d4ac71bed310967386bdb19bb5c9e9238
rriirjiuutittjjh-dot <rriirjiuutittjjh@gmail.com>
2026-09-15 04:18:13 +0200

    Delete clock-years-v1 directory

--- clock-years-v1/src/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { HomeView } from "@/components/home-view";
-
-export const Route = createFileRoute("/")({ component: Home });
-
-function Home() {
-  return <HomeView />;
-}
--- clock-years-v1/src/routes/register.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { AuthForm } from "@/components/auth/auth-form";
-
-export const Route = createFileRoute("/register")({ component: Register });
-
-function Register() {
-  return <AuthForm mode="register" />;
-}
