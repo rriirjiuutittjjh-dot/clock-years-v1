d02d4f8226c9c2c379e5b77e2bce8d80f0114a8c
rriirjiuutittjjh-dot <rriirjiuutittjjh@gmail.com>
2026-09-15 04:31:51 +0200

    Delete migrations directory

--- migrations/0003_username.sql
+++
@@ -1,3 +0,0 @@
--- better-auth username plugin: sign in with a username or an email.
-alter table "user" add column if not exists "username" text unique;
-alter table "user" add column if not exists "displayUsername" text;
